a2141f5a109c4fdea0581861dd86b0fcee70f28f
Ankit Kashyap <ankitsonukashyap@gmail.com>
2023-12-29 20:47:57 +0530

    commit

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="flex items-center gap-10 py-16">
+        <span className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500">Made by Ankit Kashyap</span>
+    </div>
+  );
+}
+
+export default Footer;
